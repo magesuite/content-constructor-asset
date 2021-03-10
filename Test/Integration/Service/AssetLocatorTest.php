3b277e01021c540958f2ad2c428e7ddc14a5afce
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2021-03-10 10:04:03 +0100

    test: [KRG-1158] Add compatibility with Magento 2.4.2

--- Test/Integration/Service/AssetLocatorTest.php
+++ Test/Integration/Service/AssetLocatorTest.php
@@ -5,52 +5,59 @@ namespace MageSuite\ContentConstructorAsset\Test\Integration\Service;
 class AssetLocatorTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @var \MageSuite\ContentConstructor\AssetLocator
-     */
-    private $locator;
-
-    /**
      * @var \Magento\TestFramework\ObjectManager
      */
-    private $objectManager;
+    protected $objectManager;
 
-    public function setUp(): void {
+    /**
+     * @var \MageSuite\ContentConstructor\AssetLocator
+     */
+    protected $locator;
+
+    public function setUp(): void
+    {
         $this->objectManager = \Magento\TestFramework\ObjectManager::getInstance();
 
         $this->locator = $this->objectManager
             ->get(\MageSuite\ContentConstructorAsset\Service\AssetLocator::class);
-
     }
 
     /**
      * @magentoAppArea adminhtml
      */
-    public function testItReturnsCorrectUrlForAdminArea() {
-        $url = $this->prepareRegexUrl('http://localhost/pub/static/version([0-9]+?)/adminhtml/Magento/backend/en_US/MageSuite_ContentConstructorAsset/images/sprites.svg');
+    public function testItReturnsCorrectUrlForAdminArea()
+    {
+        $expectedUrl = $this->prepareRegexUrl('http://localhost/static/version([0-9]+?)/adminhtml/Magento/backend/en_US/MageSuite_ContentConstructorAsset/images/sprites.svg');
 
         $assertRegExp = method_exists($this, 'assertMatchesRegularExpression') ? 'assertMatchesRegularExpression' : 'assertRegExp';
 
+        $url = $this->locator->getUrl('images/sprites.svg');
+        $url = str_replace('pub/', '', $url);
         $this->$assertRegExp(
-            $url,
-            $this->locator->getUrl('images/sprites.svg')
+            $expectedUrl,
+            $url
         );
     }
 
     /**
      * @magentoAppArea frontend
      */
-    public function testItReturnsCorrectUrlForFrontend() {
-        $url = $this->prepareRegexUrl('http://localhost/pub/static/version([0-9]+?)/frontend/Magento/luma/en_US/images/sprites.svg');
+    public function testItReturnsCorrectUrlForFrontend()
+    {
+        $expectedUrl = $this->prepareRegexUrl('http://localhost/static/version([0-9]+?)/frontend/Magento/luma/en_US/images/sprites.svg');
 
         $assertRegExp = method_exists($this, 'assertMatchesRegularExpression') ? 'assertMatchesRegularExpression' : 'assertRegExp';
 
+        $url = $this->locator->getUrl('images/sprites.svg');
+        $url = str_replace('pub/', '', $url);
         $this->$assertRegExp(
-            $url,
-            $this->locator->getUrl('images/sprites.svg')
+            $expectedUrl,
+            $url
         );
     }
 
-    protected function prepareRegexUrl($url) {
+    protected function prepareRegexUrl($url)
+    {
         $url = str_replace('/', '\/', $url);
         return sprintf('/%s/', $url);
     }
